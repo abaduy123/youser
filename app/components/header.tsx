ee899ed341ecf26f4f791e3a95fa0ee4981ae302
abaduy123 <dgikpp@gmail.com>
2024-06-20 21:26:27 +0300

    small fix

--- app/components/header.tsx
+++ app/components/header.tsx
@@ -3,7 +3,7 @@ export default function Header () {
         
         <div className="flex justify-center items-center flex-col  flex-wrap mt-40 mb-40 ">
             <p className="mb-5 text-5xl">قمة</p>
-            <p className="text-main-font-color text-5xl"> 
+            <p className="text-main-font-color text-5xl text-center"> 
                  منصة الباحثين عن التدريب  
             </p>
 
