324746fc8d02ba22a0dcac2411e17a9eccf73365
abaduy123 <58109340+abaduy123@users.noreply.github.com>
2024-06-27 20:27:16 +0300

    Update header.tsx

--- app/components/header.tsx
+++ app/components/header.tsx
@@ -2,7 +2,7 @@ export default function Header () {
     return (
         
         <div className="flex justify-center items-center flex-col  flex-wrap mt-40 mb-40 ">
-            <p className="mb-5 text-5xl">قمة</p>
+            <p className="mb-5 text-5xl">يسر</p>
             <p className="text-main-font-color text-5xl text-center"> 
                  منصة الباحثين عن التدريب  
             </p>
